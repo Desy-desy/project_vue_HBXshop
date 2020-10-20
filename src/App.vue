50fb87d1b38d7e5d8c22515367dd3d64eea0e16a
[Desy] <[136327293@qq.com]>
2020-10-20 20:48:28 +0800

    k

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <tabbar />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import changeSize from "assets/js/app_home";
+import Home from "views/home/Home";
+import Tabbar from "components/tabbar/tabbar.vue";
+import Footer from 'components/footer';
+export default {
+  name: "App",
+  components: {
+    Tabbar,
+    Footer
+  },
+};
+</script>
+
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+.app {
+  /*font-size: 100px;*/
+}
+</style>
