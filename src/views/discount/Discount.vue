78a4af61b6b85911cfa4a3787b9db364ae85cc79
[Desy] <[136327293@qq.com]>
2020-11-03 17:04:08 +0800

    hbx

---
+++ src/views/discount/Discount.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+    <News />
+    <Choose />
+    <Man />
+<!--     <Woman />-->
+    <Brand />
+	</div>
+</template>
+
+<script>
+import Choose from './choose'
+import Man from './man'
+import Woman from './woman'
+import Brand from '../brands/brand'
+import News from "../../components/news"
+	export default {
+    name: "Discount",
+    components:{
+      Choose,
+      Man,
+      Woman,
+      Brand,
+      News
+    }
+	} 
+</script>
+
+<style>
+</style>
